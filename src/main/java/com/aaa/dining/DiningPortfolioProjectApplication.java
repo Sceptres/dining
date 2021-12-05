b0eca5c30a948132d78541262027a355449b7a8a
Sceptres <abdalq2003@gmail.com>
2021-12-05 11:56:48 -0500

    Initial spring project commit.

---
+++ src/main/java/com/aaa/dining/DiningPortfolioProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aaa.dining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningPortfolioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningPortfolioProjectApplication.class, args);
+	}
+
+}

8cd38275ddb5d411d9ede3bbe3d28ebd01c3b60f
Sceptres <abdalq2003@gmail.com>
2021-12-06 13:59:16 -0500

    Add find by the restaurant id and dining review status query

--- src/main/java/com/aaa/dining/repositories/DiningReviewRepository.java
+++ src/main/java/com/aaa/dining/repositories/DiningReviewRepository.java
@@ -3,5 +3,8 @@ package com.aaa.dining.repositories;
 import com.aaa.dining.entities.DiningReview;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DiningReviewRepository extends CrudRepository<DiningReview, Integer> {
+    List<DiningReview> findByRestaurantIdAndStatus(Long restaurantId, boolean status);
 }

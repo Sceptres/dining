af4d66e3b74f85cd5147e69853c912f715d1a754
Sceptres <abdalq2003@gmail.com>
2021-12-05 22:24:18 -0500

    Implement Dining Review CRUD repository

---
+++ src/main/java/com/aaa/dining/repositories/DiningReviewRepository.java
@@ -0,0 +1,7 @@
+package com.aaa.dining.repositories;
+
+import com.aaa.dining.entities.DiningReview;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DiningReviewRepository extends CrudRepository<DiningReview, Integer> {
+}

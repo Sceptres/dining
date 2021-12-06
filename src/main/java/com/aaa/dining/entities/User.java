53015e2b8d03c6aff0da1c5504004dad3b6f804f
Sceptres <abdalq2003@gmail.com>
2021-12-05 22:18:03 -0500

    Add user entity

---
+++ src/main/java/com/aaa/dining/entities/User.java
@@ -0,0 +1,61 @@
+package com.aaa.dining.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="USER")
+public class User {
+
+    // Id
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    // The name of the user
+    @Getter
+    @Setter
+    @Column(name="NAME")
+    private String name;
+
+    // The users city
+    @Getter
+    @Setter
+    @Column(name="CITY")
+    private String city;
+
+    // The users state
+    @Getter
+    @Setter
+    @Column(name="STATE")
+    private String state;
+
+    // The users zipcode
+    @Getter
+    @Setter
+    @Column(name="ZIPCODE")
+    private String zipcode;
+
+    // Is the user interested in peanut allergies
+    @Getter
+    @Setter
+    @Column(name="PEANUT")
+    private Boolean isPeanutAllergy;
+
+    // Is the user interested in egg allergies
+    @Getter
+    @Setter
+    @Column(name="EGG")
+    private Boolean isEggAllergy;
+
+    // Is the user interested in dairy allergies
+    @Getter
+    @Setter
+    @Column(name="DAIRY")
+    private Boolean isDairyAllergic;
+
+}

eb660664729b2eaa53b45c062d3cf083f1f81b50
Sceptres <abdalq2003@gmail.com>
2021-12-05 21:56:03 -0500

    Admin review data model

---
+++ src/main/java/com/aaa/dining/models/AdminReview.java
@@ -0,0 +1,17 @@
+package com.aaa.dining.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class AdminReview {
+
+    /**
+     * A boolean representing whether
+     * the admin accepts the dining
+     * review
+     */
+    @Getter
+    @Setter
+    private Boolean isAdminAccept;
+
+}

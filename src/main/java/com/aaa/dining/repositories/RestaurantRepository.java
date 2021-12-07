ff001dfc1b10ce0afcf2efbc5258e0550d5e9d15
Sceptres <abdalq2003@gmail.com>
2021-12-07 12:59:21 -0500

    Add Restaurant exists query that checks name and zipcode

--- src/main/java/com/aaa/dining/repositories/RestaurantRepository.java
+++ src/main/java/com/aaa/dining/repositories/RestaurantRepository.java
@@ -4,4 +4,5 @@ import com.aaa.dining.entities.Restaurant;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RestaurantRepository extends CrudRepository<Restaurant, Integer> {
+    boolean existsRestaurantByNameAndZipcode(String name, String zipcode);
 }

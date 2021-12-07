2f6410c3e1bb30b0db7268a172d16c9aff6ec300
Sceptres <abdalq2003@gmail.com>
2021-12-07 13:46:50 -0500

    Add query to get restaurants by zipcode and has at least 1 review. Returned in descending order

--- src/main/java/com/aaa/dining/repositories/RestaurantRepository.java
+++ src/main/java/com/aaa/dining/repositories/RestaurantRepository.java
@@ -1,8 +1,15 @@
 package com.aaa.dining.repositories;
 
 import com.aaa.dining.entities.Restaurant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface RestaurantRepository extends CrudRepository<Restaurant, Integer> {
     boolean existsRestaurantByNameAndZipcode(String name, String zipcode);
+
+    @Query(value="SELECT r FROM Restaurant r WHERE r.zipcode=?1 AND r.peanut IS NOT NULL AND r.egg IS NOT NULL and r.dairy IS NOT NULL " +
+                 "ORDER BY r.peanut DESC, r.egg DESC, r.dairy DESC")
+    List<Restaurant> findByZipcodeAndHasReviewDesc(String zipcode);
 }

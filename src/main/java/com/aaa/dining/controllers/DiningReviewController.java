a6cb2eb7a3622c97497d52b401bfd48c560b8d6f
Sceptres <abdalq2003@gmail.com>
2021-12-12 21:50:57 -0500

    Add get dining review API endpoint

--- src/main/java/com/aaa/dining/controllers/DiningReviewController.java
+++ src/main/java/com/aaa/dining/controllers/DiningReviewController.java
@@ -2,6 +2,7 @@ package com.aaa.dining.controllers;
 
 import com.aaa.dining.entities.DiningReview;
 import com.aaa.dining.repositories.DiningReviewRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,5 +19,16 @@ public class DiningReviewController {
 
     public DiningReviewController(final DiningReviewRepository diningReviewRepository) {
         this.diningReviewRepository = diningReviewRepository;
+    }
+
+    // Gets a dining review given the database id
+    @GetMapping(path="/{reviewId}")
+    public ResponseEntity<DiningReview> getDiningReview(@PathVariable(name="reviewId") Long reviewId) {
+        Optional<DiningReview> diningReviewOptional = this.diningReviewRepository.findById(reviewId);
+
+        // Was the review not found?
+        if (diningReviewOptional.isEmpty()) return new ResponseEntity<DiningReview>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<DiningReview>(diningReviewOptional.get(), HttpStatus.OK);
     }
 }
--- src/main/java/com/aaa/dining/repositories/DiningReviewRepository.java
+++ src/main/java/com/aaa/dining/repositories/DiningReviewRepository.java
@@ -4,7 +4,9 @@ import com.aaa.dining.entities.DiningReview;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DiningReviewRepository extends CrudRepository<DiningReview, Integer> {
+    Optional<DiningReview> findById(Long id);
     List<DiningReview> findByRestaurantIdAndStatus(Long restaurantId, boolean status);
 }

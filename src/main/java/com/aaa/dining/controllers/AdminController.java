b2b0ae5e01b709dea6d2724a1fe850f1118ec117
Sceptres <abdalq2003@gmail.com>
2021-12-13 13:00:23 -0500

    Add get all pending reviews API endpoint

--- src/main/java/com/aaa/dining/controllers/AdminController.java
+++ src/main/java/com/aaa/dining/controllers/AdminController.java
@@ -1,8 +1,14 @@
 package com.aaa.dining.controllers;
 
+import com.aaa.dining.entities.DiningReview;
 import com.aaa.dining.repositories.DiningReviewRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/admin")
@@ -12,6 +18,13 @@ public class AdminController {
 
     public AdminController(final DiningReviewRepository diningReviewRepository) {
         this.diningReviewRepository = diningReviewRepository;
+    }
+
+
+    // Get all pending dining reviews
+    @GetMapping(path="/pendingReviews")
+    public ResponseEntity<List<DiningReview>> getPendingReviews() {
+        return new ResponseEntity<List<DiningReview>>(this.diningReviewRepository.findByStatus(false), HttpStatus.OK);
     }
 
 }

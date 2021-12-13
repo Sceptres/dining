12c82138d5faccb05c5a4f0e347e13603877d023
Sceptres <abdalq2003@gmail.com>
2021-12-12 22:33:22 -0500

    Add create new dining review API endpoint

--- src/main/java/com/aaa/dining/controllers/DiningReviewController.java
+++ src/main/java/com/aaa/dining/controllers/DiningReviewController.java
@@ -2,23 +2,31 @@ package com.aaa.dining.controllers;
 
 import com.aaa.dining.entities.DiningReview;
 import com.aaa.dining.repositories.DiningReviewRepository;
+import com.aaa.dining.repositories.RestaurantRepository;
+import com.aaa.dining.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(name="/reviews")
+@RequestMapping(path="/reviews")
 public class DiningReviewController {
 
     private final DiningReviewRepository diningReviewRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final UserRepository userRepository;
 
-    public DiningReviewController(final DiningReviewRepository diningReviewRepository) {
+    public DiningReviewController(
+            final DiningReviewRepository diningReviewRepository,
+            final RestaurantRepository restaurantRepository,
+            final UserRepository userRepository
+            ) {
         this.diningReviewRepository = diningReviewRepository;
+        this.restaurantRepository = restaurantRepository;
+        this.userRepository = userRepository;
     }
 
     // Gets a dining review given the database id
@@ -30,5 +38,32 @@ public class DiningReviewController {
         if (diningReviewOptional.isEmpty()) return new ResponseEntity<DiningReview>(HttpStatus.NOT_FOUND);
 
         return new ResponseEntity<DiningReview>(diningReviewOptional.get(), HttpStatus.OK);
+    }
+
+    // Adds a new dining review to the database
+    @PostMapping(path="/new")
+    public ResponseEntity<DiningReview> createDiningReview(@RequestBody DiningReview diningReview) {
+        // Is the provided info missing any information?
+        // Is the request trying to set the dining review status?
+        if (
+                Objects.isNull(diningReview.getName()) || Objects.isNull(diningReview.getRestaurantId()) ||
+                Objects.isNull(diningReview.getPeanutScore()) || Objects.isNull(diningReview.getEggScore()) ||
+                Objects.isNull(diningReview.getDairyScore()) || Objects.nonNull(diningReview.getStatus())
+        ) {
+            return new ResponseEntity<DiningReview>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Does the restaurant not exist in the database?
+        // Is the user trying to submit this review not signed up?
+        if (!this.restaurantRepository.existsById(diningReview.getRestaurantId()) || !this.userRepository.existsUserByName(diningReview.getName()))
+            return new ResponseEntity<DiningReview>(HttpStatus.NOT_FOUND);
+
+        // Did the user already review this restaurant?
+        if (this.diningReviewRepository.existsDiningReviewByNameAndRestaurantId(diningReview.getName(), diningReview.getRestaurantId()))
+            return new ResponseEntity<DiningReview>(HttpStatus.CONFLICT);
+
+        // Create the dining review
+        diningReview.setStatus(false);
+        return new ResponseEntity<DiningReview>(this.diningReviewRepository.save(diningReview), HttpStatus.CREATED);
     }
 }
